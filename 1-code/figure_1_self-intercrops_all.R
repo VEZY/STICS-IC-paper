ea2d069a5453dc4159238d90e64f8643c3d35b89
remi.vezy <VEZY@users.noreply.github.com>
2022-04-21 12:23:53 +0200

    Improve fig.1: show all species

--- 1-code/figure_1_self-intercrops_all.R
+++ 1-code/figure_1_self-intercrops_all.R
@@ -28,7 +28,7 @@ main_dir = normalizePath("0-data/usms-optim-radiative", winslash = "/")
 
 # Define the variables to simulate ----------------------------------------
 
-sim_variables = c("lai(n)","masec(n)","QNplante","mafruit", "resmes", "fapar")
+sim_variables = c("lai(n)","masec(n)","QNplante","mafruit", "hauteur", "fapar")
 
 # Which variables need to be summed for plot scale results (instead of averaged)
 to_sum = c("lai_n","masec_n","abso_n","mafruit","QNplante", "fapar")
@@ -40,11 +40,16 @@ to_sum = c("lai_n","masec_n","abso_n","mafruit","QNplante", "fapar")
 usms = 
   list(
     "Auzeville-Pea-SC" = c(sc = "SC_Pea_2005-2006_N0", ic = "IC_Pea_Pea_2005-2006_N0"),
-    "Angers-SC-Barley" = c(sc = "SC_Barley_Angers_2003_N0", ic = "IC_Barley_Barley_Angers_2003_N0")
+    "Auzeville-Wheat-SC" = c(sc = "SC_Wheat_2005-2006_N0", ic = "IC_Wheat_Wheat_2005-2006_N0"),
+    "Angers-SC-Barley" = c(sc = "SC_Barley_Angers_2003_N0", ic = "IC_Barley_Barley_Angers_2003_N0"),
+    "Auzeville_wfb-Fababean-SC" = c(sc = "Fababean_SC_2007", ic = "Fababean_IC_2007"),
+    "sojaTardif2012-SC" = c(sc = "SojaTardif-SC2012", ic = "SojaTardif-IC2012"),
+    "tourPrecoce2012-SC" = c(sc = "TournPrecoce-SC2012", ic = "TournPrecoce-IC2012")
   )
 
+# SticsRFiles::get_usms_list("0-data/usms-optim-radiative/tourPrecoce2012-SC/usms.xml")
 
-presentation = TRUE # FALSE for the paper (white background), TRUE for the presentation
+presentation = FALSE # FALSE for the paper (white background), TRUE for the presentation
 
 if(presentation){
   text_color = "white"
@@ -53,12 +58,12 @@ if(presentation){
 }
 
 res = mapply(function(x,y){
-  # x = usms[[2]]
-  # y = names(usms)[[2]]
+  # x = usms[[3]]
+  # y = names(usms)[[3]]
   workspace = file.path(main_dir, y)
   SticsRFiles::gen_varmod(workspace, sim_variables)
   SticsOnR::run_javastics(
-    javastics = javastics, workspace_path = workspace,
+    javastics = javastics, workspace = workspace,
     stics_exe = "Stics_IC_v13-01-2022.exe",
     usms_list = x
   )
@@ -73,7 +78,7 @@ res = mapply(function(x,y){
   
   
   IC_sum =
-    plots[[grep("^IC_", names(plots))]]$data%>%
+    plots[[grep("(IC_|-IC)", names(plots))]]$data%>%
     group_by(.data$Date,.data$variable)%>%
     summarise(Simulated_sum = sum(.data$Simulated),
               Simulated_mean = mean(.data$Simulated))%>%
@@ -86,7 +91,7 @@ res = mapply(function(x,y){
   
   stats = 
     left_join(
-      plots[[grep("^SC_", names(plots))]]$data,
+      plots[[grep("(SC_|-SC)", names(plots))]]$data,
       IC_sum, 
       by = c("Date","variable"), 
       suffix = c("_sc", "_ic")
@@ -120,7 +125,7 @@ res = mapply(function(x,y){
     mutate(plot_index = order(variable))
   
   p = 
-    plots[[grep("^SC_", names(plots))]]$data%>%
+    plots[[grep("(SC_|-SC)", names(plots))]]$data%>%
     mutate(System = "Sole crop")%>%
     bind_rows(.,IC_sum)%>%
     mutate(variable = 
@@ -203,4 +208,144 @@ res = mapply(function(x,y){
   return(list(plot = p, stats = stats))
 }, usms, names(usms), SIMPLIFY = FALSE)
 
+res$`Auzeville-Pea-SC`$plot
+res$`Auzeville-Wheat-SC`$plot
 res$`Angers-SC-Barley`$plot
+res$`sojaTardif2012-SC`$plot
+res$`tourPrecoce2012-SC`$plot
+res$`Auzeville_wfb-Fababean-SC`$plot
+
+
+res_scat = mapply(function(x,y){
+  # x = usms[[3]]
+  # y = names(usms)[[3]]
+  workspace = file.path(main_dir, y)
+  SticsRFiles::gen_varmod(workspace, sim_variables)
+  SticsOnR::run_javastics(
+    javastics = javastics, workspace = workspace,
+    stics_exe = "Stics_IC_v13-01-2022.exe",
+    usms_list = x
+  )
+  # Get the results
+  sim = get_sim(workspace = workspace, usm = x)
+  
+  # Get the observations
+  obs = get_obs(workspace =  workspace, usm = x)
+  # Pre-make the plot with {CroPlotR}
+  plots = plot(sim,obs=obs)
+  # Compute the sum (or average) of variables for both intercrops to match the sole crop  
+  
+  df_ic =
+    plots[[grep("(IC_|-IC)", names(plots))]]$data%>%
+    group_by(.data$Date,.data$variable)%>%
+    summarise(Simulated_sum = sum(.data$Simulated),
+              Simulated_mean = mean(.data$Simulated))%>%
+    mutate(
+      Simulated = ifelse(.data$variable %in% to_sum, Simulated_sum, Simulated_mean)
+    )%>%
+    select(-Simulated_sum,-Simulated_mean)
+  
+  df_sc = plots[[grep("(SC_|-SC)", names(plots))]]$data%>%select(-Sit_Name)
+  
+  df = 
+    left_join(
+      df_sc,
+      df_ic, 
+      by = c("Date","variable"), 
+      suffix = c("_sc", "_ic")
+    )
+  
+  return(df)
+}, usms, names(usms), SIMPLIFY = FALSE)
+
+df = bind_rows(res_scat, .id = "Crop")
+
+# Scatter plot:
+ggplot(df, aes(x = Simulated_sc , y = Simulated_ic, color = Crop))+
+  geom_abline()+
+  geom_point()+
+  facet_wrap(
+    variable~., 
+    scales = "free", 
+    labeller = label_parsed, 
+    strip.position = "left", 
+    ncol = 2
+  )+
+  ggtitle(NULL) +
+  labs(y = NULL) +
+  theme_minimal() +
+  # scale_colour_manual(values= c("Self Intercrop" = "#6EC0C0", "Sole crop" = "#746EC2"))+
+  theme(
+    legend.direction = "horizontal", 
+    legend.position = 'bottom', 
+    strip.placement.y = "outside"
+  )
+
+# Line plot:
+p = 
+  df%>%
+  mutate(
+    Crop = recode(
+      Crop,
+      "Auzeville-Pea-SC" = "bold(Pea)",
+      "Auzeville-Wheat-SC" = "bold(Wheat)",
+      "Angers-SC-Barley" = "bold(Barley)",
+      "Auzeville_wfb-Fababean-SC" = "bold(Fababean)",
+      "sojaTardif2012-SC" = "bold(Soybean)",
+      "tourPrecoce2012-SC" = "bold(Sunflower)" 
+    ),
+    variable = recode(
+      .data$variable,
+      "lai_n" = "bold(LAI~(m^{2}~m^{-2}))",
+      "masec_n" = "bold(Agb~(t~ha^{-1}))",
+      "mafruit" = "bold(Grain~(t~ha^{-1}))",
+      "QNplante"= "bold(N~acc.~(kg~ha^{-1}))",
+      "fapar"= "bold(faPAR)",
+      "hauteur"= "bold(Height~(m))"
+    )
+  )%>%
+  ggplot(aes(x = Date))+
+  geom_point(
+    aes(y = Observed), show.legend = FALSE, size = 1.5,
+    shape = 21, stroke = 1.5, colour = "#F49690", fill = "#F496904C",
+    na.rm = TRUE
+  )+
+  geom_line(aes(y = Simulated_ic, lty = "Self Intercrop", color = "Self Intercrop"), lwd = 1.3)+
+  geom_line(aes(y = Simulated_sc, lty = "Sole crop", color = "Sole crop"), lwd = 1.3)+
+  facet_grid(
+    vars(variable),
+    vars(Crop),
+    scales = "free", 
+    labeller = label_parsed
+  )+
+  scale_y_continuous(n.breaks = 5, minor_breaks = NULL)+
+  scale_x_datetime(minor_breaks = NULL)+
+  ggtitle(NULL) +
+  labs(y = NULL) +
+  theme_minimal() +
+  scale_colour_manual(values= c("Self Intercrop" = "#6EC0C0", "Sole crop" = "#746EC2"))+
+  labs(
+    colour = "Simulation:",
+    lty = "Simulation:"
+  )+
+  theme(
+    legend.position = 'bottom',
+    legend.box = 'vertical',
+    axis.title.x = element_text(face = "bold", size = 12),
+  )+
+  guides(color = guide_legend(nrow = 1, byrow = TRUE))
+
+p
+
+ggsave(p, filename = "Fig.1_self-intercrop_all.png", path = "2-outputs/plots",
+       width = 18, height = 18, units = "cm")
+
+# Statistics for each variable for all crops:
+df%>%
+  group_by(variable,Crop)%>%
+  summarise(
+    nRMSE = nRMSE(Simulated_ic, Simulated_sc),
+    EF = EF(Simulated_ic, Simulated_sc),
+    Bias = Bias(Simulated_ic, Simulated_sc)
+  )
+
